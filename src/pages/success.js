fb7beeb5ffdb56adcbe8a2f547032d3318ac6bd0
Sam Wellander <samwellander@gmail.com>
2019-03-04 19:04:08 -0500

    add success page for after form submit

---
+++ src/pages/success.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { Link } from "gatsby";
+import {
+  withStyles,
+  Typography,
+  Grid,
+  Button,
+  Divider
+} from "@material-ui/core";
+
+const styles = {
+  wrapper: {
+    display: "flex",
+    minHeight: "100vh",
+    flexDirection: "column"
+  },
+  successContainer: {
+    flex: 1
+  },
+  success: {
+    textAlign: "center"
+  },
+  message: {
+    paddingBottom: 40
+  }
+};
+
+const SuccessPage = ({ classes }) => (
+  <div className={classes.wrapper}>
+    <Header splash={false} />
+    <Grid
+      className={classes.successContainer}
+      container
+      justify="center"
+      alignItems="center"
+    >
+      <div className={classes.success}>
+        <Typography className={classes.message} variant="display1">
+          Your message was sent!
+        </Typography>
+        <div>
+          <Button component={Link} to="/" variant="outlined">
+            Back
+          </Button>
+        </div>
+      </div>
+    </Grid>
+    <Footer />
+  </div>
+);
+
+export default withStyles(styles)(SuccessPage);
